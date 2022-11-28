0b52b2b9c8df9c0698286f57e127dbfaa9dbc00e
Anish Raj <a.rajv11@gmail.com>
2022-11-27 19:28:22 -0500

    added order logic

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
-import { isAuth } from '../utils.js';
+import { isAuth } from '../../frontend/src/utils.js';
 
 const orderRouter = express.Router();
 orderRouter.post(
@@ -31,6 +31,29 @@ orderRouter.get(
     const order = await Order.findById(req.params.id);
     if (order) {
       res.send(order);
+    } else {
+      res.status(404).send({ message: 'Order Not Found' });
+    }
+  })
+);
+
+orderRouter.put(
+  '/:id/pay',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.email_address,
+      };
+
+      const updatedOrder = await order.save();
+      res.send({ message: 'Payment Completed', order: updatedOrder });
     } else {
       res.status(404).send({ message: 'Order Not Found' });
     }
